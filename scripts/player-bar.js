421fb0a8634d005e2b8956e7be7c327bee7934d2
Andrew Allen <w.andrewallen@gmail.com>
2017-10-14 14:06:45 -0500

    Checkpoint 13 Bloc Jams- completed

---
+++ scripts/player-bar.js
@@ -0,0 +1,29 @@
+$( document ).ready(function() {
+  $('button#play-pause').click( function() {
+    player.playPause();
+    $(this).attr('playState', player.playState);
+
+  });
+
+  $('button#next').click( function(){
+    if (player.playState !== 'playing') { return; }
+
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const nextSongIndex = currentSongIndex + 1;
+    if (nextSongIndex >= album.songs.length) { return; }
+    const nextSong = album.songs[nextSongIndex];
+    player.playPause(nextSong);
+  });
+
+  $('button#previous').click( function(){
+    if (player.playState !== 'playing') { return; }
+
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const prevSongIndex = currentSongIndex - 1;
+    if (prevSongIndex >= album.songs.length) { return; }
+    const prevSong = album.songs[prevSongIndex];
+    player.playPause(prevSong);
+  });
+
+
+});
